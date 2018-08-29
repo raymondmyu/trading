11a4213fdb56cfaa2f6c1b02901a5b7a698e76f9
Raymond Yu <raymond.m.yu@gmail.com>
2018-08-29 03:18:24 +0000

    Added merge_jsons.py

---
+++ intrinio/merge_jsons.py
@@ -0,0 +1,13 @@
+import json
+import os, glob
+import tqdm
+from multiprocessing import Pool
+
+requests = []
+def get_request(f):
+    return dict(filename=os.path.basename(f), contents=json.load(open(f,'r')))
+
+p = Pool(3)
+requests = p.map(get_request, glob.glob('financials/requests/*.json'))
+
+json.dump(requests,open('financials/merged/merged1.json','w'))
