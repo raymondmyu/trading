c7ba699ea5f9753abeff7982bd5cd2bd2e08de4e
Raymond Yu <raymond.m.yu@gmail.com>
2018-09-01 10:34:05 -0500

    fixed logic around tickers_in_sector

--- intrinio/run_api_daily.py
+++ intrinio/run_api_daily.py
@@ -10,15 +10,12 @@ requests_coll = db.requests
 tickers = get_percentile_filtered_df()
 
 # Find the sectors that have been entirely accounted for
+symbols_in_requests = requests_coll.find().distinct('contents.identifier')
 sectors = []
 for sector in tickers.Sector.unique():
     tickers_in_sector = tickers.query("Sector==@sector").Symbol.unique().tolist()
-    # Are there any symbols NOT in tickers_in_sector?
-    if requests_coll.find({
-            'contents.identifier': {
-                '$nin': tickers_in_sector
-            }
-        }).count() > 0:
+    # Are there any tickers_in_sector NOT in the database already?
+    if len(set(tickers_in_sector)-set(symbols_in_requests))>0:
         print('Sector',sector,'is incomplete')
         sectors.append(sector)
 
