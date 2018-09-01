dfac859cb16edd301be74382404e569db52924f0
Raymond Yu <raymond.m.yu@gmail.com>
2018-09-01 17:03:41 -0500

    fixed unzip to zip

--- intrinio/mongo/requests_to_longform.sh
+++ intrinio/mongo/requests_to_longform.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 /home/ray/anaconda3/bin/python requests_to_longform.py
 cd /home/ray/Projects/trading/intrinio/mongo/data/db
-/usr/bin/mongoexport --type=csv -d financials -c requests -o longform.csv -f tag,value,ticker,statement,date
-unzip longform.zip longform.csv
+/usr/bin/mongoexport --type=csv -d financials -c tocsv -o longform.csv -f tag,value,ticker,statement,date
+rm longform.zip
+zip longform.zip longform.csv
