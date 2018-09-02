29935f76eb28072066a22fb8dca43725717e7923
Raymond Yu <raymond.m.yu@gmail.com>
2018-09-02 02:25:34 +0000

    finally got requests_to_longform.sh working

--- intrinio/mongo/requests_to_longform.sh
+++ intrinio/mongo/requests_to_longform.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /home/ray/anaconda3/bin/python requests_to_longform.py
 cd /home/ray/Projects/trading/intrinio/mongo/data/db
+rm -f longform.csv
 /usr/bin/mongoexport --type=csv -d financials -c tocsv -o longform.csv -f tag,value,ticker,statement,date
-rm longform.zip
+rm -f longform.zip
 zip longform.zip longform.csv
